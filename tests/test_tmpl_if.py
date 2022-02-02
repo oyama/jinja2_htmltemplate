8d1cb42c75ffe95f04a334548a8304aea57136a3
Danil <9011501+difeid@users.noreply.github.com>
2022-02-02 18:04:09 +0300

    Added support VALUE for TMPL_IF statements

--- tests/test_tmpl_if.py
+++ tests/test_tmpl_if.py
@@ -14,3 +14,10 @@ def test_tmpl_else():
     eq_(t.from_string(
         '<TMPL_IF NAME="cond">True<TMPL_ELSE>False</TMPL_IF>'),
         '{% if cond %}True{% else %}False{% endif %}', msg='TMPL_IF')
+
+
+def test_tmpl_if_value():
+    t = HtmlTemplate()
+    eq_(t.from_string(
+        '<TMPL_IF NAME="cond" VALUE="value">True</TMPL_IF>'),
+        '{% if cond == "value" %}True{% endif %}', msg='TMPL_IF')
